3af10b8d2dfb66bc156d73c0aa39c8eadcb42bde
Lorena <criado.manzaneque@gmail.com>
2020-07-22 13:24:43 +0200

    javascript
    con array de colores

--- javascript1.js
+++ javascript1.js
@@ -3,11 +3,16 @@ document.getElementById("btn").addEventListener("click", cambiarColor);
 let pixelColor = document.querySelectorAll(".pixel");
  
 function cambiarColor(){
-pixelColor.forEach(element => element.style.backgroundColor = "yellow");
+    let arrayColores = ['#851de0','#aa26da','#f1fa3c','#05dfd7','#723881','#c355f5','#08ffc8', '#f4ff61'];
+    let numeroRandom = Math.floor(8 * Math.random());
+    for (let i = 0; i < pixelColor.length; i++){
+    pixelColor[i].style.background = arrayColores[numeroRandom]; 
+    }
+
 }
 
 
 
-
-
-
+// function cambiarColor(){
+// pixelColor.forEach(element => element.style.backgroundColor = "yellow");
+// }
